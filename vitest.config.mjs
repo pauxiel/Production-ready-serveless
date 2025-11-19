d2b3706eeefbd45515ef6c3b1ac2657112289236
pauxiel <paul.scholes50@yahoo.com>
2025-11-19 10:38:08 -0500

    wrote integration test

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    globalSetup: ['./tests/steps/init.mjs'],
     include: ['**/*.test.mjs'],
     exclude: ['**/node_modules/**', '**/dist/**'],
     environment: 'node'
