9f232ce5f1f28cafd4b87efb6b8e6b38c25497ce
pauxiel <paul.scholes50@yahoo.com>
2025-12-07 15:33:50 -0500

    forgotten

--- tests/test_cases /place-order.test.mjs
+++ tests/test_cases /place-order.test.mjs
@@ -1,30 +1,26 @@
-import { describe, it, expect, beforeAll, afterAll, vi } from 'vitest'
+import { describe, it, expect, beforeAll, afterAll } from 'vitest'
 import * as when from '../steps/when.mjs'
 import * as given from '../steps/given.mjs'
 import * as teardown from '../steps/teardown.mjs'
-import { EventBridgeClient } from '@aws-sdk/client-eventbridge'
-
-const mockSend = vi.fn()
-EventBridgeClient.prototype.send = mockSend
+import { startListening } from '../messages.mjs'
 
 describe('Given an authenticated user', () => {
-  let user
+  let user, listener
 
   beforeAll(async () => {
     user = await given.an_authenticated_user()
+    listener = startListening()
   })
 
   afterAll(async () => {
     await teardown.an_authenticated_user(user)
+    await listener.stop()
   })
 
   describe(`When we invoke the POST /orders endpoint`, () => {
     let resp
 
     beforeAll(async () => {
-      mockSend.mockClear()
-      mockSend.mockReturnValue({})
-
       resp = await when.we_invoke_place_order(user, 'Fangtasia')
     })
 
@@ -32,21 +28,22 @@ describe('Given an authenticated user', () => {
       expect(resp.statusCode).toEqual(200)
     })
 
-    if (process.env.TEST_MODE === 'handler') {
-      it(`Should publish a message to EventBridge bus`, async () => {
-        expect(mockSend).toHaveBeenCalledTimes(1)
-        const [ putEventsCmd ] = mockSend.mock.calls[0]
-        expect(putEventsCmd.input).toEqual({
-          Entries: [
-            expect.objectContaining({
-              Source: 'big-mouth',
-              DetailType: 'order_placed',
-              Detail: expect.stringContaining(`"restaurantName":"Fangtasia"`),
-              EventBusName: process.env.bus_name
-            })
-          ]
-        })
+    it(`Should publish a message to EventBridge bus`, async () => {
+      const { orderId } = resp.body
+      const expectedMsg = JSON.stringify({
+        source: 'big-mouth',
+        'detail-type': 'order_placed',
+        detail: {
+          orderId,
+          restaurantName: 'Fangtasia'
+        }
       })
-    }
+
+      await listener.waitForMessage(x => 
+        x.sourceType === 'eventbridge' &&
+        x.source === process.env.bus_name &&
+        x.message === expectedMsg
+      )
+    }, 10000)
   })
 })
