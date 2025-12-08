f5eaca475bb0416a252cc82a840ee42fe24be811
pauxiel <paul.scholes50@yahoo.com>
2025-12-08 08:29:53 -0500

    fix search-resturaant, so github test work

--- tests/test_cases /search-restaurants.test.mjs
+++ tests/test_cases /search-restaurants.test.mjs
@@ -1,9 +1,21 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, beforeAll, afterAll } from 'vitest'
 import * as when from '../steps/when.mjs'
+import * as given from '../steps/given.mjs'
+import * as teardown from '../steps/teardown.mjs'
 
 describe(`When we invoke the POST /restaurants/search endpoint with theme 'cartoon'`, () => {
+  let user
+
+  beforeAll(async () => {
+    user = await given.an_authenticated_user()
+  })
+
+  afterAll(async () => {
+    await teardown.an_authenticated_user(user)
+  })
+
   it(`Should return an array of 4 restaurants`, async () => {
-    let res = await when.we_invoke_search_restaurants('cartoon')
+    let res = await when.we_invoke_search_restaurants('cartoon', user)
 
     expect(res.statusCode).toEqual(200)
     expect(res.body).toHaveLength(4)
